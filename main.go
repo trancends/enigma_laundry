3462f29500d375e940af67262003f3a120df5ee9
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-12-05 17:40:59 +0700

    try to insert data to mst_customer

--- main.go
+++ main.go
@@ -15,9 +15,9 @@ const (
 	dbname   = "enigma_laundry"
 )
 
-func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
+func main() {
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
@@ -27,7 +27,21 @@ func main() {
 	err = db.Ping()
 	if err != nil {
 		panic(err)
+	} else {
+		fmt.Println("Succesfully Connected!")
 	}
 
-	fmt.Println("Succesfully Connected!")
+	sqlStatement := `INSERT INTO mst_customer (id, name, phone,active_member,join_date)
+    VALUES ('C001','Sari Ayunda','081234567999','true','2020-02-23')`
+	result, err := db.Exec(sqlStatement)
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Succesfully INSERT Data")
+	}
+
+	fmt.Println(result)
 }
+
+func addCustomer(id, name, gender, phone, active_member, join_date string)
