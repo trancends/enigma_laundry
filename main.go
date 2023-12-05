3af8b142e580cf0b4f1b283de6c12604b110117c
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-12-05 19:09:50 +0700

    add delete func for customer and create connect func to tidy the codebase

--- main.go
+++ main.go
@@ -21,30 +21,21 @@ var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmod
 
 func main() {
 	customer := entity.Customer{
-		Id:            "C003",
-		Name:          "Nikola Tesla",
-		Phone:         "081234789555",
+		Id:            "C004",
+		Name:          "Albert Einstein",
+		Phone:         "081234789222",
 		Active_member: true,
-		Join_date:     time.Date(2022, 12, 12, 0, 0, 0, 0, time.Local),
+		Join_date:     time.Date(2023, 11, 11, 0, 0, 0, 0, time.Local),
 		Gender:        "M",
 	}
-	// addCustomer(customer)
-	updateCustomer(customer)
+	addCustomer(customer)
+	// updateCustomer(customer)
+	// deleteCustomer("C003")
 }
 
 func addCustomer(customer entity.Customer) {
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
+	db := connectDB()
 	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("Succesfully Connected!")
-	}
 
 	sqlStatement := `INSERT INTO mst_customer (id, name, phone,active_member,join_date,gender) 
     VALUES ($1,$2,$3,$4,$5,$6);`
@@ -61,18 +52,8 @@ func addCustomer(customer entity.Customer) {
 }
 
 func updateCustomer(customer entity.Customer) {
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
+	db := connectDB()
 	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("Succesfully Connected!")
-	}
 
 	sqlStatement := `UPDATE mst_customer SET name = $2, phone = $3, active_member = $4, 
   join_date = $5, gender = $6 WHERE id = $1`
@@ -86,4 +67,36 @@ func updateCustomer(customer entity.Customer) {
 	}
 
 	fmt.Println(result)
+}
+
+func deleteCustomer(id string) {
+	db := connectDB()
+	defer db.Close()
+
+	sqlStatement := `DELETE FROM mst_customer WHERE id = $1`
+	result, err := db.Exec(sqlStatement, id)
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Succesfully DELETED Data")
+	}
+
+	fmt.Println(result)
+}
+
+func connectDB() *sql.DB {
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Succesfully Connected!")
+	}
+
+	return db
 }
