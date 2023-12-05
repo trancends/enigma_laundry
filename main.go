840751054ae05769cc23c36d3660d069291117cd
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-12-05 18:52:44 +0700

    add update function for mst_customer

--- main.go
+++ main.go
@@ -23,12 +23,13 @@ func main() {
 	customer := entity.Customer{
 		Id:            "C003",
 		Name:          "Nikola Tesla",
-		Phone:         "081234789666",
+		Phone:         "081234789555",
 		Active_member: true,
 		Join_date:     time.Date(2022, 12, 12, 0, 0, 0, 0, time.Local),
 		Gender:        "M",
 	}
-	addCustomer(customer)
+	// addCustomer(customer)
+	updateCustomer(customer)
 }
 
 func addCustomer(customer entity.Customer) {
@@ -46,13 +47,42 @@ func addCustomer(customer entity.Customer) {
 	}
 
 	sqlStatement := `INSERT INTO mst_customer (id, name, phone,active_member,join_date,gender) 
-    VALUES ('$1','$2','$3','$4','$5','$6');`
-	result, err := db.Exec(sqlStatement, customer.Id, customer.Name, customer.Phone, customer.Active_member, customer.Gender)
+    VALUES ($1,$2,$3,$4,$5,$6);`
+	result, err := db.Exec(sqlStatement, customer.Id, customer.Name, customer.Phone,
+		customer.Active_member, customer.Join_date, customer.Gender)
 
 	if err != nil {
 		panic(err)
 	} else {
 		fmt.Println("Succesfully INSERT Data")
+	}
+
+	fmt.Println(result)
+}
+
+func updateCustomer(customer entity.Customer) {
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Succesfully Connected!")
+	}
+
+	sqlStatement := `UPDATE mst_customer SET name = $2, phone = $3, active_member = $4, 
+  join_date = $5, gender = $6 WHERE id = $1`
+	result, err := db.Exec(sqlStatement, customer.Id, customer.Name, customer.Phone,
+		customer.Active_member, customer.Join_date, customer.Gender)
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Succesfully Updated Data")
 	}
 
 	fmt.Println(result)
